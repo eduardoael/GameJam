f18d3b72ef7d041e0dfd21d41a2e08c03fa68d81
Lena <32245081+yarenlen@users.noreply.github.com>
2019-10-04 21:59:01 +0200

    basic NavMeshAgent pathfinding and patrolling waypoints

---
+++ Assets/Scripts/AIMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIMovement : MonoBehaviour
+{
+    NavMeshAgent agent;
+    public Transform[] waypoints;
+    private int nextWaypoint;
+    
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        GoToNextWaypoint();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (agent.remainingDistance < .5f)
+        {
+            GoToNextWaypoint();
+        }
+    }
+
+    private void GoToNextWaypoint()
+    {
+        if (waypoints.Length == 0)
+        {
+            return;
+        }
+        
+        //set new waypoint
+        agent.destination = waypoints[nextWaypoint].position;
+       
+        //find next waypoint           
+        if (nextWaypoint <= waypoints.Length)
+        {
+            nextWaypoint++;
+        }
+        else
+        {
+            nextWaypoint = 0;
+        }
+    }
+}
