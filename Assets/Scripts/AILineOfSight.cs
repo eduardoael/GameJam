1e199ad8ef2b61c00459ba8b0af95dda57b9db01
Lena <32245081+yarenlen@users.noreply.github.com>
2019-10-05 10:53:42 +0200

    line of sight

---
+++ Assets/Scripts/AILineOfSight.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AILineOfSight : MonoBehaviour
+{
+
+    NavMeshAgent agent;
+    LayerMask mask; 
+    public float viewingDistance = 10;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        mask = LayerMask.GetMask("Player"); //"Player" Layer
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 rayOrigin = transform.position + (Vector3.up * 1.3f);
+       
+        RaycastHit hit;
+        if (Physics.Raycast(rayOrigin, transform.TransformDirection(Vector3.forward), out hit, viewingDistance, mask))
+        {
+            Debug.DrawRay(rayOrigin, transform.TransformDirection(Vector3.forward) * hit.distance, Color.green);
+            print("hit");
+        }
+        else 
+        {
+            Debug.DrawRay(rayOrigin, transform.TransformDirection(Vector3.forward) * viewingDistance, Color.yellow);
+        }
+
+
+    }
+}
---
+++ Assets/Scripts/AIMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIMovement : MonoBehaviour
+{
+    NavMeshAgent agent;
+    
+    public Transform[] waypoints;
+    private int nextWaypoint;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        GoToNextWaypoint();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (agent.remainingDistance < .2f)
+        {
+            GoToNextWaypoint();
+        }
+    }
+
+    private void GoToNextWaypoint()
+    {
+        if (waypoints.Length == 0)
+        {
+            Debug.Log("No waypoints set for" + this.gameObject.name);
+            return;
+        }
+
+        //set new waypoint
+        agent.destination = waypoints[nextWaypoint].position;
+
+        //find next waypoint   
+        nextWaypoint = (nextWaypoint + 1) % waypoints.Length;
+        
+    }
+}
