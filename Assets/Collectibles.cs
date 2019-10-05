aac21d8481f49705fdf87380e7241b02aeea8ac4
Lena <32245081+yarenlen@users.noreply.github.com>
2019-10-05 17:09:02 +0200

    collectibles collectible with t

---
+++ Assets/Collectibles.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectibles : MonoBehaviour
+{
+
+    public Canvas collectiblesPrompt;
+    private bool isCollectible;
+
+    private void Start()
+    {
+        collectiblesPrompt.gameObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        collectiblesPrompt.gameObject.SetActive(true);
+        isCollectible = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        collectiblesPrompt.gameObject.SetActive(false);
+        isCollectible = false;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T) && isCollectible)
+        {
+            Debug.Log("DESTROYED Clipboard");
+            Destroy(gameObject);
+        }
+    }
+}
