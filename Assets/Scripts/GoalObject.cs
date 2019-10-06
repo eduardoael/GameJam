d7ac763b798c82baa706714b99b6918614e7b52e
eduardoael <eduardoesqueche@gmail.com>
2019-10-06 13:32:20 +0200

    Scenes with Videos for 0,1,2 and 3 Clipboards

---
+++ Assets/Scripts/GoalObject.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalObject : MonoBehaviour
+{
+    public GameController gameController;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            gameController.TerminalReached();
+        }
+    }
+}
